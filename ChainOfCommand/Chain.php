3e6043e09de074a7d7e772f1d26697f13ef72c37
Software 0044 <software0044@users.noreply.github.com>
2016-05-30 18:43:39 +0200

    0.2

--- ChainOfCommand/Chain.php
+++ ChainOfCommand/Chain.php
@@ -1,5 +1,53 @@
 <?php
-
-	
-
-?>
+ 
+abstract class Logger
+{
+    private $_next = null;
+    public function setNext(Logger $logger)
+    {
+        $this->_next = $logger;
+        return $this->_next;
+    }
+    public function log($message)
+    {
+        $this->_log($message);
+        if ($this->_next !== null) {
+            $this->_next->log($message);
+        }
+    }
+    abstract protected function _log($message);
+}
+class EmailLogger extends Logger
+{
+    public function _log($message)
+    {
+        echo "Sending via email: ", $message, " \n<br>";
+    }
+}
+class ErrorLogger extends Logger
+{
+    protected function _log($message)
+    {
+        echo "Sending to stderr: ", $message, " \n<br>";
+    }
+}
+class StdoutLogger extends Logger
+{
+    protected function _log($message)
+    {
+        echo "Writing to stdout: ", $message, " \n<br>";
+    }
+}
+ 
+ 
+/**
+ * Usage
+ */
+$logger = new StdoutLogger();
+$logger->setNext(new ErrorLogger())->setNext(new EmailLogger());
+$logger->log('Something happened');
+ 
+// Output:
+// Writing to stdout: Something happened
+// Sending to stderr: Something happened
+// Sending via email: Something happened
---
+++ ChainOfCommand/Chain2.php
@@ -0,0 +1,54 @@
+<?php
+
+	interface ICommand 
+	{
+		function onCommand($name, $args);
+	}
+	
+	class CommandChain
+	{
+		private $commands = array();
+		
+		public function addCommand($cmd)
+		{
+			$this->commands[] = $cmd;
+		}
+		
+		public function runCommand($name, $args)
+		{
+			foreach ($this->commands as $cmd)
+			{
+				if ($cmd->onCommand($name, $args))
+					return;
+			}
+		}
+	}
+	
+	class UserCommand implements ICommand
+	{
+		public function onCommand($name, $args)
+		{
+			if ($name != "addUser") return false;
+			echo "UserCommand handling addUser\n";
+			return true;
+		}
+	}
+	
+	class MailCommand implements ICommand	
+	{
+		public function onCommand($name, $args)
+		{
+			if ($name != "mail") return false;
+			echo "MailCommand handling mail\n";
+			return true;
+		}		
+	}
+	
+	$cc = new CommandChain();
+	$cc->addCommand(new UserCommand());
+	$cc->addCommand(new MailCommand());
+	
+	$cc->runCommand('addUser', null);
+	$cc->runCommand('mail', null);
+
+?>
